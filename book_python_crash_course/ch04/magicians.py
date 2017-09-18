63436cc427a1de3cb809b968ee82bd6eb24b4244
keith brooks <kbrook10@gmail.com>
2017-09-18 14:52:11 -0400

    Adding exercises from chapter 3 and 4 on working with lists

---
+++ book_python_crash_course/ch04/magicians.py
@@ -0,0 +1,17 @@
+# (1) Using 'for' loops to avoid repetition in code and prevent unnecessary code changes.
+# the below for loop prints each magician in the list magicians
+
+magicians = []
+
+magicians.append('alice')
+magicians.append('david')
+magicians.append('carolina')
+
+for magician in magicians:
+	# print(magician)
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+print("Thank you everyone, that was a great magic show!")
+
+
+# (2) Trying it yourself
